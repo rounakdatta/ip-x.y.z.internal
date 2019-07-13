081bc7ae487ce4c3c601f1c77645b00718a099f3
rounakdatta <rounak.datta@hotstar.com>
2019-07-14 03:03:32 +0530

    correct parsing; fix infinite redirection

--- js/background.js
+++ js/background.js
@@ -1,5 +1,23 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'js/inject.js'
-	});
-});
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+		if (!details.url.startsWith("http://ip-")) {
+			return;
+		}
+
+		var currentURL = details.url.replace(/(^\w+:|^)\/\//, '');
+		var firstColon = currentURL.indexOf(':');
+		var firstDot = currentURL.indexOf('.');
+		var actualIP = currentURL.substr(0, firstDot);
+		actualIP = actualIP.substr(actualIP.indexOf("-") + 1).replace(/-/g, ".");
+		var remainingIP = currentURL.substr(firstColon);
+
+		return {redirectUrl: ("http://" + actualIP + remainingIP)};
+    },
+    {
+        urls: [
+			"*://*:*/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
